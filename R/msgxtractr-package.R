6e20d8a9dc9306bd411091bff92b619acb2da576
boB Rudis <bob@rud.is>
2017-08-25 22:09:27 -0400

    docs

--- R/msgxtractr-package.R
+++ R/msgxtractr-package.R
@@ -1,5 +1,9 @@
 #' Read Outlook '.msg' Files
 #'
+#' 'Microsoft' 'Outlook' messages can be saved in '.msg' files. Tools are provided that
+#' enable extraction of metadata, envelope, headers, body and attachments from these
+#' files.
+#'
 #' @name msgxtractr
 #' @docType package
 #' @author Bob Rudis (bob@@rud.is)
